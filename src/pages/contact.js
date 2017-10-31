fc0de366d21ce2116e878a8448c5f123afa1ac62
Tom Vorel <tomvorel13@gmail.com>
2017-10-31 13:43:32 +0100

    Changing typo in contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact = () => (
   <div className="contact__div animated fadeIn">
-    <h1 className="contact__h1">Contact page</h1>
+    <h1 className="contact__h1">Contact</h1>
     <p className="contact__p">
       For any enquiries, feel free to contact me at{" "}
       <a href="mailto:tomvorel13@gmail.com">tomvorel13@gmail.com</a>
