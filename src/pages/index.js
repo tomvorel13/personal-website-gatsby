d61a18217dea64e18688bac1903789d6c72dd713
Tom Vorel <tomvorel13@gmail.com>
2017-11-03 09:26:57 +0100

    Changing the color scheme and font

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const IndexPage = () => (
   <div className="homepage__div animated fadeIn">
-    <h1 className="homepage__h1">Front End Developer</h1>
+    <h1 className="homepage__h1">Front End Developer & Designer</h1>
     <p className="homepage__p">
-      I am a self taught front end web developer, with the main focus on React
-      and its ecosystem.
+      I am a self taught front end web developer and designer with passion for
+      creating awesome things!
     </p>
   </div>
 );
